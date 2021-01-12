fa5ed11383b1125feb0246a734d664e0ff57c316
1234tgk <1234tgk@gmail.com>
2021-01-12 16:05:24 -0500

    Contact plus minor tweak

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+    return (
+        <div className='contact' id='contact'>
+            <h2>CONTACT INFO</h2>
+            <div className='contact-container'>
+                <h5>Phone Number</h5>
+                <h6>(1) 111-222-3344</h6>
+                <h5>Email Address</h5>
+                <h6>1234tgk (dot) ca (at) gmail (dot) com</h6>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
